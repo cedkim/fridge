4ad351bef0d4f6cb0028b042bad0f7a8bd1f749d
Cedric Kim <hello@cedric.today>
2020-07-05 16:28:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,12 +34,29 @@ client.on('message', (message) => {
     const user = message.mentions.users.first() || message.author;
     message.reply("yeah", {files: [user.displayAvatarURL]});
   }else if(message.content === "fridge help me please i cant breathe" || message.content === "fridge" || message.content === "fridge help" || message.content === "!fridge" || message.content === "how do you use fridge") {
-    message.channel.send("Fridge has come to help someone.\nElmo has come to eat someone.\n-----\n'smart fridges should be able to scream' makes me scream\n'open the potato compartment' opens my potato compartment\n'nice face' shows you the guy trapped inside of me\n'is my face nice' shows you your avatar\n'i love baked potatoes' sings a song\n'i want some milk' shows you milk\n'put a banana in the fridge' reminds you that you're not supposed to put bananas in the fridge\n'fridge help me please i cant breathe' shows you this (you can also use 'fridge help' or '!fridge')");
+    message.channel.send("Fridge has come to help someone.\nElmo has come to eat someone.\n-----\n'smart fridges should be able to scream' makes me scream\n'open the potato compartment' opens my potato compartment\n'nice face' shows you the guy trapped inside of me\n'is my face nice' shows you your avatar\n'i love baked potatoes' sings a song\n'i want some milk' shows you milk\n'put a banana in the fridge' reminds you that you're not supposed to put bananas in the fridge\n'buy fridge+' lets you purchase Fridge+ or Ghost Fridge\n'fridge help me please i cant breathe' shows you this (you can also use 'fridge help' or '!fridge')");
   }else if(message.content === "i want some milk"){
     message.channel.send("M I L K", {files: ["https://i.ibb.co/4MgvtTP/Fresh-milk-on-wooden-table-in-morning-light-and-nature-background.jpg"]});
   }else if(message.content === "put a banana in the fridge"){
     message.channel.send("Why would you? Bananas actually go bad faster in the fridge. A simple explanation is that bananas grow in warm areas, so putting them in a cooler area makes them go bad faster. Lol.");
-  }
+  }else if(message.content === "buy fridge+"){
+		message.channel.send({embed: {
+			title: "💸 Buy Fridge+/Ghost Fridge",
+			description: "Thanks for choosing to financially support Fridgebot! There are two premium subscriptions: Fridge+ if you want to personalize Fridgebot, and Ghost Fridge if you want a completely custom bot, hosted on the Fridgebot infrastructure.",
+			fields: [
+				{
+			    name: "Payment",
+			    value: "Fridge+ costs 2$ a month\nGhost Fridge costs 3$ a month\n[💳 Continue with a credit card](https://gumroad.com/l/fridgeplus)",
+		    }
+			],
+			image: {
+				url: "https://i.ibb.co/2jrzh2P/Fridge-Illustration.png"
+			},
+			footer: {
+		    text: "✔️ Your payment will be securely processed by Gumroad"
+	    }
+		}});
+	}
 });
 
 client.login(process.env.THE_KEY_TO_THE_LOCKED_FRIDGE);

6d1f808379ee193a6474a02c8b68b38ff03109ae
Cedric Kim <hello@cedric.today>
2020-07-06 08:39:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,7 +40,7 @@ client.on('message', (message) => {
   }else if(message.content === "put a banana in the fridge"){
     message.channel.send("Why would you? Bananas actually go bad faster in the fridge. A simple explanation is that bananas grow in warm areas, so putting them in a cooler area makes them go bad faster. Lol.");
   }else if(message.content === "buy fridge+"){
-    message.channel.send({embed: {"title":"💸 Buy Fridge+/Ghost Fridge","description":"Thanks for choosing to financially support Fridgebot! There are two premium subscriptions: Fridge+ if you want to personalize Fridgebot, and Ghost Fridge if you want a completely custom bot, hosted on the Fridgebot infrastructure.","fields":[{"name":"Payment","value":"Fridge+ costs 2$ a month\nGhost Fridge costs 3$ a month\n[Learn more](https://gumroad.com/l/fridgeplus)"}],"footer":{"text":"✔️ Your payment will be securely processed by Gumroad"},"image":{"url":"https://i.ibb.co/2jrzh2P/Fridge-Illustration.png"}});
+    message.channel.send({embed: {"title":"💸 Buy Fridge+/Ghost Fridge","description":"Thanks for choosing to financially support Fridgebot! There are two premium subscriptions: Fridge+ if you want to personalize Fridgebot, and Ghost Fridge if you want a completely custom bot, hosted on the Fridgebot infrastructure.","fields":[{"name":"Payment","value":"Fridge+ costs 2$ a month\nGhost Fridge costs 3$ a month\n[Learn more](https://gumroad.com/l/fridgeplus)"}],"footer":{"text":"✔️ Your payment will be securely processed by Gumroad"},"image":{"url":"https://i.ibb.co/2jrzh2P/Fridge-Illustration.png"}}});
   }
 });
 
